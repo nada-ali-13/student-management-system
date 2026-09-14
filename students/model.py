e8de9581c740bbb991ca30d8d3d13ce790171633
Dhekra <dhekraalhusseini@gmail.com>
2026-09-14 22:42:54 +0300

    Add Student model

---
+++ students/model.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, student_id, name, age, email, phone):
+        self.id = student_id
+        self.name = name
+        self.age = age
+        self.email = email
+        self.phone = phone
