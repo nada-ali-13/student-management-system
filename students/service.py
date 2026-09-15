5ca3610035d005c54de1065a38852aebe410fa62
Dhekra <dhekraalhusseini@gmail.com>
2026-09-15 11:09:29 +0300

    Add add_student service

--- students/service.py
+++ students/service.py
@@ -5,3 +5,34 @@ def get_all_students():
         Student.from_dict(student)
         for student in data
     ]
+    
+def add_student(name, age, email, phone):
+
+    validate_name(name)
+    validate_age(age)
+    validate_email(email)
+    validate_phone(phone)
+
+    students = get_all_students()
+
+    new_id = max(
+        [student.id for student in students],
+        default=0
+    ) + 1
+
+    student = Student(
+        new_id,
+        name,
+        age,
+        email,
+        phone
+    )
+
+    students.append(student)
+
+    save_data(
+        STUDENTS_FILE,
+        [student.to_dict() for student in students]
+    )
+
+    return student
