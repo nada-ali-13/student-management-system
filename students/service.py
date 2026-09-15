c08fce3e956f0ef62399465df052189ee19ba783
Dhekra <dhekraalhusseini@gmail.com>
2026-09-15 11:23:22 +0300

    Add get_student_by_id service

--- students/service.py
+++ students/service.py
@@ -36,3 +36,44 @@ def add_student(name, age, email, phone):
     )
 
     return student
+
+
+def get_student_by_id(student_id):
+    students = get_all_students()
+
+    for student in students:
+        if student.id == student_id:
+            return student
+
+    return None
+
+def add_student(name, age, email, phone):
+
+    validate_name(name)
+    validate_age(age)
+    validate_email(email)
+    validate_phone(phone)
+
+    students = get_all_students()
+
+    new_id = max(
+        [student.id for student in students],
+        default=0
+    ) + 1
+
+    student = Student(
+        new_id,
+        name,
+        age,
+        email,
+        phone
+    )
+
+    students.append(student)
+
+    save_data(
+        STUDENTS_FILE,
+        [student.to_dict() for student in students]
+    )
+
+    return student
